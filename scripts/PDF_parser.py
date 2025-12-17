b5f0efd443436bed1312626db79dda8e46f0d420
AresBlue <224150682+AresBlue@users.noreply.github.com>
2025-12-18 01:52:04 +1030

    Update PDF_parser.py
    Added a sanity checker, just so unchecked PDFs don't get mutated without user's knowledge.

--- scripts/PDF_parser.py
+++ scripts/PDF_parser.py
@@ -1,5 +1,5 @@
 import PyPDF2
-import re, json
+import re, json, random
 def pdf_parser():
     with open("TOPIK1vocab.pdf", "rb") as f:
         reader = PyPDF2.PdfReader(f)
@@ -19,6 +19,13 @@ def pdf_parser():
         entries = re.findall(r'(\d+)([^\d]+?)\s([^\d]+?)(?=\d|$)', line)
         for entry in entries:
             learning_words.append({"korean": entry[1].strip(), "english": entry[2].strip()})
-
+    sample = imported_pdf_wordlist[random.randint(0, len(imported_pdf_wordlist)-1]
+    user_input = input(f"If the following is similar to this: 'korean': '경기장', 'english': 'stadium, arena' \nThen enter Y, else, PDF not compatable with parser.\n{sample}\nY/n:")
+    
+if user_input.lower() != "y":
+    print("Warning: The import may have formatting issues.")
+else:
+    print("Import looks good!")
     with open("learning_vocab.json", "w", encoding="utf-8") as f:
+
         json.dump(learning_words, f, ensure_ascii=False, indent=2)
