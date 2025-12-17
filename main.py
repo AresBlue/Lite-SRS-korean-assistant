39e1e5de7b6fad344e1a3357476d0de0dc231499
AresBlue <224150682+AresBlue@users.noreply.github.com>
2025-12-18 04:44:47 +1030

    Update main.py
    making it flow better, instead of multiple mutations of halfmem, I am making it just the one in main.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from scripts import Message, wordlist_state, io_handler, session
 
 def main():
     wrong_words = []
+    right_words = []
     SESSION_SIZE = 34 #set to whatever you want, future me :)
 
     remembered_words, halfmem, lst_total_length, loaded_words = io_handler.import_backup()
@@ -15,13 +16,19 @@ def main():
     print("--------------------"*4)
 
 
-    remembered_words, halfmem, wrong_words = interim_session(remembered_words, halfmem, wrong_words)
+    remembered_words, right_words, wrong_words = interim_session(remembered_words, right_words, wrong_words)
 
 
     with open("remembered_words.json", "w", encoding="utf-8") as f:
         json.dump(remembered_words, f, ensure_ascii=False, indent=2)
 
     halfmem += wrong_words
+
+    for word in good_words:
+    try:
+        halfmem.remove(word)
+    except ValueError:
+        pass
 
     with open("halfmem.json", "w", encoding="utf-8") as f:
         json.dump(halfmem, f, ensure_ascii=False, indent=2)
@@ -48,3 +55,4 @@ def main():
 if __name__ == "__main__":
 
     main()
+
