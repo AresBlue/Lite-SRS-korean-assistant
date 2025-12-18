86d6df64e193c5246a0c6792655bcca193e7556b
AresBlue <224150682+AresBlue@users.noreply.github.com>
2025-12-19 01:22:39 +1030

    Update wordlist_state.py
    didn't place in scripts folder.

--- scripts/wordlist_state.py
+++ scripts/wordlist_state.py
@@ -1,9 +1,12 @@
+import time
 def word_list_check(SESSION_SIZE, lst_length, halfmem):
     mem_length = len(halfmem)
-    if lst_length + mem_length < SESSION_SIZE:
+    if lst_length == 0 and mem_length == 0:
+        print("wordlist empty, please delete learning_vocab.json and relaunch from bat/bash.")
+        time.sleep(5)
+        exit()
+    elif lst_length + mem_length < SESSION_SIZE:
         print("Congrats, this is your final session for this word-list!")
         return lst_length + mem_length
-    elif lst_length + mem_length == 0:
-        print("No words in wordlist, would you like ") #going to add in the pdf parser hook here.
     else:
         return SESSION_SIZE
