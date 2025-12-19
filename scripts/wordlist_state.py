52b8223e38cda6da150bc7ff46de2186d42a6d7b
AresBlue <224150682+AresBlue@users.noreply.github.com>
2025-12-19 21:16:10 +1030

    Update wordlist_state.py
    Increased time given for empty wordlist alert

--- scripts/wordlist_state.py
+++ scripts/wordlist_state.py
@@ -3,12 +3,13 @@ def word_list_check(session_size, lst_length, halfmem):
     mem_length = len(halfmem)
     if lst_length == 0 and mem_length == 0:
         print("wordlist empty, please delete learning_vocab.json and relaunch from bat/bash.")
-        time.sleep(5)
+        time.sleep(15)
         exit()
     elif lst_length + mem_length < session_size:
         print("Congrats, this is your final session for this word-list!")
         return lst_length + mem_length
     else:
         return session_size
+
 
 
