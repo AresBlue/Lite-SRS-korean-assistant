a82341f987af727cc75b0ee77aad3af0a58b8153
AresBlue <224150682+AresBlue@users.noreply.github.com>
2025-12-19 16:43:12 +1030

    Update main.py
    externalized config and added a hold on session end to prevent the script from auto closing.

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
-import json
+import json, time
 from scripts import Message, wordlist_state, io_handler, session
+from config import config
 
 def main():
     wrong_words = []
     right_words = []
-    SESSION_SIZE = 34 #set to whatever you want, future me :)
+    session_size = config["session_size"]
 
     remembered_words, halfmem, lst_total_length, loaded_words = io_handler.import_backup()
 
@@ -15,9 +16,7 @@ def main():
     Message.message_of_today(lst_length, lst_total_length, len(halfmem))
     Message.spacer()
 
-
     remembered_words, right_words, wrong_words = session.interim_session(remembered_words, right_words, wrong_words)
-
 
     with open("remembered_words.json", "w", encoding="utf-8") as f:
         json.dump(remembered_words, f, ensure_ascii=False, indent=2)
@@ -35,10 +34,8 @@ def main():
 
     available_words = [w for w in loaded_words if w not in remembered_words and w not in halfmem]
 
-
-    SESSION_SIZE = wordlist_state.word_list_check(SESSION_SIZE, lst_length, halfmem)
-
-    today_session, available_words = session.session(SESSION_SIZE, remembered_words, halfmem, available_words)
+    session_size = wordlist_state.word_list_check(session_size, lst_length, halfmem)
+    today_session, available_words = session.session(session_size, remembered_words, halfmem, available_words)
 
     with open("interim.json", "w", encoding="utf-8") as f:
         json.dump(today_session, f, ensure_ascii=False, indent=2)
@@ -50,9 +47,19 @@ def main():
     for i, w in enumerate(today_session, start=1):
         print(f"{i}: {w['korean']} - {w['english']}")
 
+    try:
+        Message.spacer()
+        print("To quit session, please Ctrl+C...\n")
+        while True:
+            time.sleep(0.1)
+    except KeyboardInterrupt:
+        print("Exiting SRS learning assistant, please review today's words...")
+        time.sleep(1)
+
 if __name__ == "__main__":
 
     main()
+
 
 
 
