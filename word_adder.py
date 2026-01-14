065b2d8061541815d8c5616f2d310f8dc5da1d4e
BlueAres <224150682+AresBlue@users.noreply.github.com>
2026-01-15 04:13:03 +1030

    Improved word_adder.py. still terrible but it's for testing

--- word_adder.py
+++ word_adder.py
@@ -6,13 +6,9 @@ def word_adder():
 
     def word_finder(wordlist, user_word):
         for word in wordlist:
-            if user_word == word["english"]:
+            if user_word in word["english"]:
                 return word
         return None
-
-    def interim_save(interim_data):
-        with open("interim.json", "w", encoding="utf-8") as f:
-            json.dump(interim_data, f, ensure_ascii=False, indent=2)
 
     remembered_words, halfmem, _, loaded_words = io_handler.import_backup()
     sources = [loaded_words, halfmem, remembered_words]
@@ -26,31 +22,54 @@ def word_adder():
 
             user_word = input("Enter your word: ").strip().lower()
 
-            if word_finder(interim_data, user_word):
-                print("Word already in interim_data.")
-                return
+            found_words = []
 
-            for source in sources:
+            for i, source in enumerate(sources):
                 found_word = word_finder(source, user_word)
 
                 if found_word:
-                    interim_data.append(found_word)
+                    print(f"Word found! it is: {found_word}")
 
-                    print(f"Word found! word is: {found_word} and has been added to interim_data.")
+                    found_words.append((found_word, source))
 
-                    if source is loaded_words:
-                        source.remove(found_word)
 
-                        with open("learning_vocab.json", "w", encoding="utf-8") as f:
-                            json.dump(loaded_words, f, ensure_ascii=False, indent=2)
-
-                    break
-
-            else:
-                translation = input("Word not present anywhere... please give translation: ").strip()
+            if found_words == []:
+                print("No words found...")
+                translation = input("Please give translation: ").strip()
                 interim_data.append({"english": user_word, "translation": translation})
 
-            interim_save(interim_data)
+            else:
+                print("Found word(s):")
+                for i, (w, _) in enumerate(found_words, start=1):
+                    print(f"{i}: {w['korean']} - {w['english']}")
+
+                choice = input("Please choose which word you'd like to add by it's number: ")
+                try: choice = int(choice)
+                except ValueError:
+                    choice = input("Please enter a number: ")
+
+                if choice < 1 or choice > len(found_words):
+                    choice = int(input("Stay within bounds of the possible word choices please: "))
+
+                word, source = found_words[choice - 1]
+
+                if word in interim_data:
+                    print("Word already in interim_data.")
+                    return
+
+                if source is loaded_words:
+                    loaded_words.remove(word)
+
+                    with open("learning_vocab.json", "w", encoding="utf-8") as f:
+                        json.dump(loaded_words, f, ensure_ascii=False, indent=2)
+
+                else:
+                    pass
+
+                interim_data.append(word)
+
+            with open("interim.json", "w", encoding="utf-8") as f:
+                json.dump(interim_data, f, ensure_ascii=False, indent=2)
 
     else:
         print("Interim.json not found, ending.")
