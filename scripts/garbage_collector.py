72d98759fffcf0e8378dbb3df8cbbdbd2a3798f7
AresBlue <224150682+AresBlue@users.noreply.github.com>
2026-01-26 17:37:11 +1030

    Update garbage_parser to return cleaned words
    The garbage_parser function now returns a list of cleaned words, filtering out empty entries.

--- scripts/garbage_collector.py
+++ scripts/garbage_collector.py
@@ -2,9 +2,16 @@ from config import english_garbage_words, korean_garbage_words
 
 def garbage_parser(loaded_words):
 
+    cleaned_words = []
+
     for word in loaded_words:
+
         for garbage in english_garbage_words:
             word['english'] = word['english'].replace(garbage, '').strip()
         for garbage in korean_garbage_words:
             word['korean'] = word['korean'].replace(garbage, '').strip()
-    return loaded_words
+
+        if word['english'] and word['korean']:
+            cleaned_words.append(word)
+
+    return cleaned_words
